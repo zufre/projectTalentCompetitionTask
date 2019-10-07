8e27833a70407cf8bdafef4b4387f4a837c87d01
Cédric David <cedric.david1974@gmail.com>
2019-10-07 12:06:45 +1000

    Create JobSummaryCard.jsx

---
+++ JobSummaryCard.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Cookies from 'js-cookie';
+import { Popup, Pagination, Icon, Dropdown, Checkbox, Accordion, Form, Segment, Card, Label, Button, CardContent  } from 'semantic-ui-react';
+import moment from 'moment';
+
+export class JobSummaryCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.selectJob = this.selectJob.bind(this)
+    }
+
+    selectJob(id) {
+        var cookies = Cookies.get('talentAuthToken');
+        //url: 'http://localhost:51689/listing/listing/closeJob',
+    }
+
+    render() {
+        let currentDate = new Date();
+        console.log(this.props.createdOn)
+        return (
+        <Card style={{ width: '35rem', height: '30rem' }}>
+            <Card.Content>
+                    <Card.Header>{this.props.jobTitle}</Card.Header>
+                <Label color='black' ribbon='right'><Icon name='user' />0</Label>
+                    <Card.Meta>{this.props.location}, {this.props.country}</Card.Meta>
+                <Card.Description>
+                        {this.props.description}
+                </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+
+
+                <span >
+                    <div style={{ height: '100%' }} className='left floated' >
+                            { this.props.date  < currentDate.toISOString() ?
+                            <Label color='red'  >Expired</Label>
+                            :
+                            <Label color='blue' >Unexpired</Label>
+                        }
+                    </div>
+                    <div style={{ width: '70%' }} className='right floated ui three buttons'>
+                        <Button key={1} basic color='blue' onClick={this.onClickCloseJobs} ><Icon name="close" />Close</Button>
+                        <Button basic color='blue'><Icon name="edit" />Edit</Button>
+                        <Button basic color='blue'><Icon name="copy" />Copy</Button>
+                    </div>
+                </span>
+            </Card.Content>
+            </Card>
+            )
+    }
+}
